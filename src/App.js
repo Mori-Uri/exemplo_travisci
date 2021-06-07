0656f3e53614e079f8aac8b117ec43d938c08ceb
Uriel Mori <68873798+Mori-Uri@users.noreply.github.com>
2021-06-07 08:23:08 -0300

    Mensagem alterada

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Esse troço tá funcionando ?
+          Olha aqui o site funcinando !
         </p>
         <a
           className="App-link"
