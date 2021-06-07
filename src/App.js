59177b28c8a1039c9d8c7363563be51f3724ad77
Uriel Mori <68873798+Mori-Uri@users.noreply.github.com>
2021-06-07 08:50:19 -0300

    finalv2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://i.imgur.com/H48KBe2.jpg" className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Olha aqui o site funcinando !
         </p>
